0319bfacddb9b64ec6de3b4d188380f3652cd81d
ilhan <ilhanodun34@gmail.com>
2023-11-27 22:56:57 +0300

    ADDED Homework0

---
+++ InstanceFieldProperty/Homework0.cs
@@ -0,0 +1,30 @@
+﻿
+class Student
+{
+    protected string Name;
+    public int Age { get; set; }
+    private int height;
+    internal void SetName(string name)
+    {
+        Name = name;
+    }
+    internal string GetInfo()
+    {
+        return $"Name: {Name}, Age: {Age}. Height: {height}";
+
+    }
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student();
+        student.SetName("John");
+        student.Age = 20;
+        //student.height = 180;
+        Console.WriteLine(student.GetInfo());
+    }
+}
+
