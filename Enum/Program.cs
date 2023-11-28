66afe2e5cdea1fd9ebf4d4bea559302410e8a59b
ilhan <ilhanodun34@gmail.com>
2023-11-28 23:00:15 +0300

    ADDED Enum example

---
+++ Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿
+enum Days
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+class Program
+{
+    static void Main()
+    {
+        Days day = Days.Monday;
+        Console.WriteLine(day);
+        Console.WriteLine((int)day);
+
+        day = Days.Friday;
+        Console.WriteLine(day);
+        Console.WriteLine((int)day);
+
+        day = Days.Sunday;
+        Console.WriteLine(day);
+        Console.WriteLine((int)day);
+
+        
+    }
+}
