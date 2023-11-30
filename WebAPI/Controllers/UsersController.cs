638e46f2fe8fd1b06ee7322845a436f2dccbcfcb
ilhan <ilhanodun34@gmail.com>
2023-11-30 04:57:37 +0300

    ADDED comment lines for desc

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -2,67 +2,75 @@
 using WebAPI.Source.Business;
 
 namespace WebAPI.Controllers;
-
+/*
+* Bu controllerda CRUD operasyonları için gerekli metodlar bulunmaktadır.
+* İlgili sınıfın tanımı ve operasyonları için WebAPI/Source/Business/UserManager.cs dosyasına bakabilirsiniz.
+*/
 [ApiController]
 [Route("[controller]")]
 public class UsersController : ControllerBase
 {
     IUserService _userService;
-        public UsersController(IUserService userService)
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    // ! Burada GetAll metodunu kullanarak tüm kullanıcıları listeliyoruz.
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        var result = _userService.GetAll();
+        if (result==null)
         {
-            _userService = userService;
+            return BadRequest();
         }
-        [HttpGet("getall")]
-        public IActionResult GetAll()
+        else
         {
-            var result = _userService.GetAll();
-            if (result==null)
-            {
-                return BadRequest();
-            }
-            else
-            {
-                return Ok(result);
-            }
-            
-        }
-        [HttpGet("getbyid")]
-        public IActionResult GetById(int id)
-        {
-            var result = _userService.GetById(id);
-            if (result == null) {  return BadRequest(); }
             return Ok(result);
         }
-        [HttpPost("add")]
-        public IActionResult Add(User user)
-        {
-            if(_userService.GetAll().Contains(user)) {
-                return BadRequest();
-            } else {
-                _userService.Add(user);
-                return Ok(); 
-            }
+        
+    }
+    // ! Burada GetById metodunu kullanarak bir kullanıcıyı buluyoruz.
+    [HttpGet("getbyid")]
+    public IActionResult GetById(int id)
+    {
+        var result = _userService.GetById(id);
+        if (result == null) {  return BadRequest(); }
+        return Ok(result);
+    }
+    // ! Burada Add metodunu kullanarak bir kullanıcı ekliyoruz.
+    [HttpPost("add")]
+    public IActionResult Add(User user)
+    {
+        if(_userService.GetAll().Contains(user)) {
+            return BadRequest();
+        } else {
+            _userService.Add(user);
+            return Ok(); 
         }
-        [HttpPost("delete")]
-        public IActionResult Delete(User user)
-        {
-            if(!_userService.GetAll().Contains(user)) {
-                return BadRequest();
-            } else {
-                _userService.Delete(user);
-                return Ok(); 
-            }
+    }
+    // ! Burada Delete metodunu kullanarak bir kullanıcı siliyoruz.
+    [HttpPost("delete")]
+    public IActionResult Delete(User user)
+    {
+        if(!_userService.GetAll().Contains(user)) {
+            return BadRequest();
+        } else {
+            _userService.Delete(user);
+            return Ok(); 
         }
-        [HttpPost("update")]
-        public IActionResult Update(User user)
-        {
-            if(_userService.GetAll().Contains(user)) {
-                return BadRequest();
-            } else {
-                _userService.Update(user);
-                return Ok(); 
-            }
+    }
+    // ! Burada Update metodunu kullanarak bir kullanıcıyı güncelliyoruz.
+    [HttpPost("update")]
+    public IActionResult Update(User user)
+    {
+        if(_userService.GetAll().Contains(user)) {
+            return BadRequest();
+        } else {
+            _userService.Update(user);
+            return Ok(); 
         }
+    }
 
 }
 
--- WebAPI/Source/Business/UserManager.cs
+++ WebAPI/Source/Business/UserManager.cs
@@ -2,33 +2,35 @@ using System.Collections.Generic;
 namespace WebAPI.Source.Business
 {
     public class UserManager : IUserService
-{
+    {
+        // !! Fake data
         List<User> _userDal = new List<User>
         {
             new User(firstName: "Ilhan", lastName: "Odun", email:"ilhan@mail.com","123456),")
         };
+        // ! Ekleme senaryosu
         public void Add(User user)
         {
             _userDal.Add(user);
             System.Console.WriteLine("User added");
         }
-
+        // ! Silme senaryosu
         public void Delete(User user)
         {
             _userDal.Remove(user);
             System.Console.WriteLine("User deleted");
         }
-
+        // ! Tüm kullanıcıları listeleme senaryosu
         public List<User> GetAll()
         {
             return _userDal;
         }
-
+        // ! Id'ye göre kullanıcı bulma senaryosu
         public User GetById(int id)
         {
             return _userDal.Find(u => u.Id == id);
         }
-
+        // ! Güncelleme senaryosu
         public void Update(User user)
         {
             User userToUpdate = _userDal.Find(u => u.Id == user.Id);
