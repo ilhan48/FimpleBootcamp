4693dfe9e31f02222326c445ba879fadd4e8602e
ilhan <ilhanodun34@gmail.com>
2023-11-30 06:45:40 +0300

    ADDED logic for required

--- WebAPI/Source/Entities/User.cs
+++ WebAPI/Source/Entities/User.cs
@@ -4,10 +4,10 @@ namespace WebAPI.Source.Entities;
 public class User
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public  string Email { get; set; }
-    public  string Password { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
 
 
     public User(string firstName, string lastName, string email, string password)
